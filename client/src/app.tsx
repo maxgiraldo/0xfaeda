0d7a671382b97638cfa216c806312896854b7470
Max Giraldo <maxgiraldo@hey.com>
2023-09-02 08:43:25 -0400

    feat(client): transform grass to soil

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,33 +1,66 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { Signal, signal } from '@preact/signals'
+
+import grass01 from '/Grass01.png'
+import grass02 from '/Grass02.png'
+import grass03 from '/Grass03.png'
+import grass04 from '/Grass04.png'
+import grass05 from '/Grass05.png'
+import grass06 from '/Grass06.png'
+import grass07 from '/Grass07.png'
+import grass08 from '/Grass08.png'
+import grass09 from '/Grass09.png'
+import Soil00 from '/Soil00.png'
 import './app.css'
 
+const tileSrc1 = signal('Grass01')
+const tileSrc2 = signal('Grass02')
+
 export function App() {
-  const [count, setCount] = useState(0)
+  // TODO: make Soil look good depending on which one clicked
+  const getTileImg = (tileSrc: string) => {
+    switch (tileSrc) {
+      case 'Grass01':
+        return grass01
+      case 'Grass02':
+        return grass02
+      case 'Grass03':
+        return grass03
+      case 'Grass04':
+        return grass04
+      case 'Grass05':
+        return grass05
+      case 'Grass06':
+        return grass06
+      case 'Grass07':
+        return grass07
+      case 'Grass08':
+        return grass08
+      case 'Grass09':
+        return grass09
+      case 'Soil00':
+        return Soil00
+      default:
+        return grass01
+    }
+  }
+
+  const setNextTileSrcValue = (tileSrc: Signal<string>) => {
+    const isGrass = ['Grass01', 'Grass02', 'Grass03', 'Grass04', 'Grass05', 'Grass06', 'Grass07', 'Grass08', 'Grass09'].some((grass) => grass == tileSrc)
+    if (isGrass) {
+      tileSrc.value = 'Soil00';
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+      <div class="container">
+        <div class={"tile"} onClick={() => setNextTileSrcValue(tileSrc1)}>
+          <img src={getTileImg(tileSrc1.value)} width="100" />
+        </div>
+        <div class={"tile"} onClick={() => setNextTileSrcValue(tileSrc2)}>
+          <img src={getTileImg(tileSrc2.value)} width="100" />
+        </div>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }
